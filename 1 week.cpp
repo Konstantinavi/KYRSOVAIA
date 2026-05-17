2b7c7d0032a05d0e509d6176812dde792c307786
Матвеюк Антонина <Matvejk_AI_25@student.grsu.by>
2026-05-18 00:17:55 +0300

    Добавлены функции FindPidInCpuHistory, AddPidToCpuHistory
    Добавлены функции FindPidInCpuHistory, AddPidToCpuHistory для проверки наличия процесса в истории CPU и добавления процесса в историю

--- 1 week.cpp
+++ 1 week.cpp
@@ -8,6 +8,29 @@ ProcessCpuHistory* cpuHistory = nullptr;
 int cpuHistoryCount = 0;
 int cpuHistoryCapacity = 0;
 
+int FindPidInCpuHistory(unsigned long pid) {
+    for (int i = 0; i < cpuHistoryCount; i++) {
+        if (cpuHistory[i].pid == pid) return i;
+    }
+    return -1;
+}
+
+int AddPidToCpuHistory(unsigned long pid) {
+    if (cpuHistoryCount >= cpuHistoryCapacity) {
+        int newCap = (cpuHistoryCapacity == 0) ? 100 : cpuHistoryCapacity * 2;
+        ProcessCpuHistory* newArr = new ProcessCpuHistory[newCap];
+        for (int i = 0; i < cpuHistoryCount; i++) {
+            newArr[i] = cpuHistory[i];
+        }
+        delete[] cpuHistory;
+        cpuHistory = newArr;
+        cpuHistoryCapacity = newCap;
+    }
+    cpuHistory[cpuHistoryCount].pid = pid;
+    cpuHistory[cpuHistoryCount].lastKernel = 0;
+    cpuHistory[cpuHistoryCount].lastUser = 0;
+    return cpuHistoryCount++;
+}
 int main(){
   retirn 0;
 }
