b864bd42d2bbabfe6e9534c317fa6e110269a912
Шаститко Милана <Shastitko_MD_25@student.grsu.by>
2026-05-17 23:21:01 +0300

    Добавлены библиотеки и структуры
    Добавили библиотеки и структуры необходимые для разработчиков на 1 неделе

--- Common.h
+++ Common.h
@@ -1,1 +1,30 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
+#include <tlhelp32.h>
+#include <psapi.h>
+#include <string>
+
+struct ProcessInfo {
+    unsigned long pid;
+    wchar_t* name = nullptr;
+    size_t memoryUsage = 0;
+    double cpuUsage = 0.0;
+    double diskUsage = 0.0;
+    double netUsage = 0.0;
+    ProcCat category = CAT_BACKGROUND;
+    wchar_t* statusStr = nullptr;
+
+    unsigned long long lastReadBytes = 0;
+    unsigned long long lastWriteBytes = 0;
+    unsigned long long lastNetBytes = 0;
+    unsigned long long lastUpdateTime = 0;
+};
+
+struct ProcessCpuTime {
+    unsigned long pid;
+    unsigned long long lastKernel;
+    unsigned long long lastUser;
+    bool isActive;
+};
 
